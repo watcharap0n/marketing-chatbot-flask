93e6fecd3300a19cad878c72ded984365b079d9d
watcharap0n <wera.watcharapon@gmail.com>
2021-09-19 18:27:44 +0700

    test

--- routes/api_cors.py
+++ routes/api_cors.py
@@ -9,7 +9,6 @@ from config.db import MongoDB
 from features_line.flex_message import flex_notify_channel
 from routes.wh_notify import line_bot_api_notify
 from environ.client_environ import MONGODB_URI
-from machine_leanning.model_spam import model_spam
 import os
 
 public = Blueprint('public', __name__, template_folder='templates')
@@ -58,7 +57,6 @@ def key_model_transaction(item: dict, channel: str, userId=None, email_private=N
     if item['product'] == 'Mango ERP (Real Estate)': item['product'] = 'RealEstate'
     if item['product'] == 'Pusit (Consulting)': item['product'] = 'Consulting'
     if item['company'] == 'google': item['tag'] = ['spam']
-    item['tag'] = model_spam(item['message'])
     return item
 
 
@@ -85,9 +83,8 @@ def get_demo():
         email = item['email']
         message = item['message']
         company = item['company']
-        tag = item['tag']
         del item['_id']
-        if company == 'google' or tag == ['spam']:
+        if company == 'google':
             return jsonify(item)
         else:
             condition_message(channel, date, time, company, name, tel, email, product, message)
