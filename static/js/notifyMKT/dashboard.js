6a8972f4742b4318f030af43ab07c77029c82759
watcharap0n <wera.watcharapon@gmail.com>
2021-09-30 14:14:24 +0700

    test

--- static/js/notifyMKT/dashboard.js
+++ static/js/notifyMKT/dashboard.js
@@ -2,77 +2,93 @@ new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-        products: ['RealEstate', 'Project Planning', 'CSM', 'QCM', 'Maintenance', 'Rental', 'MRP'],
-        validCheck: [v => !!v || 'กรุณาคลิกเพื่อไปต่อ'],
-        validSelect: [v => !!v || 'กรุณาเลือกผลิตภัณฑ์'],
-        validEmail: [
-            v => !!v || 'กรุณากรอกอีเมล',
-            v => /.+@.+\..+/.test(v) || 'กรุณากรอกอีเมลให้ถูกต้อง',
+        roleList: false,
+        search: '',
+        roles: ['ADMIN', 'MEMBER', 'STAY'],
+        selectedItem: '',
+        selectedList: 0,
+        headers: [
+            {text: 'Image', value: 'img', align: 'right', width: 50},
+            {text: 'Info', value: 'display_name', align: 'center', width: 100},
+            {text: 'Approval', value: 'approval_status', align: 'center', width: 120},
+            {text: 'Role', value: 'role', align: 'center'},
+            {text: 'Model', value: 'model', align: 'center', width: 50},
+            {text: 'Date', value: 'date', align: 'center'},
         ],
-        validTel: [
-            v => !!v || 'กรุณากรอกเบอร์โทร',
-            v => (v && v.length <= 10) || 'เบอร์โทรกรอกไม่ครบ',
-        ],
-        validOther: [v => !!v || 'กรุณากรอกข้อมูลให้ครบถ้วน'],
-        formElement: {
-            name: '',
-            email: '',
-            company: '',
-            tel: '',
-            product: 'Construction',
-            other: '',
-            message: '',
-            userId: '',
-            email_private: '',
-            profile: '',
-            picture: '',
-            channel: 'LINE'
-        },
-        valid: false,
-        spinBtn: true,
+        transaction: [],
+        user: {}
+
     },
     delimiters: ["[[", "]]"],
-    created() {
-        liff.init({liffId: '1655208213-k48wpvK9'}, () => {
-                if (liff.isLoggedIn()) {
-                    liff.getProfile()
-                        .then((profile) => {
-                            console.log(liff.getContext());
-                            this.formElement.userId = profile.userId
-                            this.formElement.profile = profile.displayName
-                            this.formElement.picture = profile.pictureUrl
-                            this.formElement.email_private = liff.getDecodedIDToken().email
-                        })
-                } else {
-                    liff.login();
-                }
-            }
-        )
+    async mounted() {
+        await this.initializedLIFF()
     },
     methods: {
-        onSubmit() {
-            let validate = this.$refs.form.validate()
-            if (validate === true) {
-                this.spinBtn = false
-      const path = '/api/all/questionnaire'
-                axios.post(path, this.formElement)
-                    .then(() => {
-                        this.spinBtn = true
-                        this.popUp()
-                        this.$refs.form.reset()
-                    })
-                    .catch((err) => {
-                        this.spinBtn = true
-                         Swal.fire("มีบางอย่างผิดพลาด", "กรุณาลองใหม่อีกครั้งค่ะ", "error").then(() => {
-                            liff.closeWindow();
-                        })
-                    })
-            }
+        async validationSave() {
+            const path = `/MKT/notify/users/${this.user.user_id}/save`
+            await axios.get(path)
+                .then((res) => {
+                    console.log(res.data)
+                    this.validationUser()
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         },
-        popUp() {
-            Swal.fire("ข้อมูลบันทึกเรียบร้อย!", "เจ้าหน้าที่ได้รับข้อมูลของท่านแล้ว\nและจะดำเนินการติดต่อกลับให้เร็วที่สุด", "success").then(() => {
-                liff.closeWindow();
-            })
+        async validationUser() {
+            console.log(this.user)
+            const path = `/MKT/notify/users/${this.user.user_id}/validation`
+            await axios.get(path)
+                .then((res) => {
+                    console.log(res.data)
+                    this.initialized()
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         },
-    }
+        async initialized() {
+            const path = '/MKT/notify/users'
+            await axios.get(path)
+                .then((res) => {
+                    this.transaction = res.data
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        },
+        initializedLIFF() {
+            liff.init({liffId: '1655208213-k48wpvK9'}, () => {
+                    if (liff.isLoggedIn()) {
+                        liff.getProfile()
+                            .then((profile) => {
+                                console.log(profile)
+                                this.user.user_id = profile.userId
+                                this.user.display_name = profile.displayName
+                                this.user.img = profile.pictureUrl
+                                this.user.email = liff.getDecodedIDToken().email
+                                console.log(this.user)
+                            })
+                    } else {
+                        liff.login();
+                    }
+                }
+            )
+        },
+        roleUpdate(item, role) {
+            item.role = role
+            this.updateObj(item)
+        },
+        updateObj(item) {
+            const path = `/MKT/notify/users/${item.user_id}/obj/notify`
+            axios.put(path, item)
+                .then((res) => {
+                    console.log(res.data)
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
+        },
+
+    },
 })
